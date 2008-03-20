8463496c1568696c94a7ae7bdf8f3897ee5cc8cf
Uwe L. Korn <uwelk@xhochy.org>
2008-03-20 18:36:37 +0000

    Added license information to all Ruby files.

--- rainpress/packer_test.rb
+++ rainpress/packer_test.rb
@@ -1,8 +1,40 @@
+##############################
+# Test for Rainpress::Packer #
+##############################
+#
+#--
+# Copyright (c) 2007-2008 Uwe L. Korn
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+#++
+#
+# Author:: Uwe L. Korn <uwelk@xhochy.org>
+# Copyright:: Copyright (c) 2007-2008 Uwe L. Korn
+# License:: MIT-style License (see Rainpress)require 'test/unit'
+
 require 'test/unit'
 require File.join(File.dirname(__FILE__), 'packer.rb')
 
 module Rainpress
   # This class keeps all Tests for the class Rainpress::Packer
+  #
+  # Author:: Uwe L. Korn <uwelk@xhochy.org>
   class TestPacker < Test::Unit::TestCase
 		
 	# Create an instace of Rainpress::Packer for use in the tests
